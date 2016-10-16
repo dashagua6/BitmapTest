d2a9066cb25005c4dd0e59219f40b2eb0a53dbdc
dashagua6 <chengccycc@163.com>
2016-10-16 17:12:49 +0400

    Initial commit

---
+++ app/src/main/java/bid/woou/bitmaptest/MainActivity.java
@@ -0,0 +1,69 @@
+package bid.woou.bitmaptest;
+
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    String[] images = null;
+    AssetManager assets = null;
+    int currentImg = 0;
+    ImageView image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.image);
+        try {
+            assets = getAssets();
+            // 获取/assets/目录下的所有文件
+            images = assets.list("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        final Button next = (Button) findViewById(R.id.bt);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 如果发生数组越界
+                if (currentImg >=images.length) {
+                    currentImg = 0;
+                }
+                // 找到下一个图片文件
+                while (!images[currentImg].endsWith(".png")
+                        && !images[currentImg].endsWith(".jpg")
+                        && !images[currentImg].endsWith(".gif")) {
+                    currentImg++;
+                    // 如果已发生数组越界
+                    if (currentImg >= images.length) {
+                        currentImg = 0;
+                    }
+                }
+                InputStream assetFile = null;
+                try {
+                    // 打开指定资源对应的输入流
+                    assetFile = assets.open(images[currentImg++]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) image.getDrawable();
+                // 如果图片还未回收，先强制回收该图片
+                if (bitmapDrawable != null
+                        && !bitmapDrawable.getBitmap().isRecycled()) {
+                    bitmapDrawable.getBitmap().recycle();
+                }
+                //改变ImageView显示的图片
+                image.setImageBitmap(BitmapFactory.decodeStream(assetFile));
+            }
+        });
+
+    }
+}
